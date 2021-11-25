bed2cbd20400b2a0a9673f12948a406a6487a0cb
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-11-25 22:42:25 +0500

    Done bhai

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
+// import AccountCircle from '@mui/icons-material/AccountCircle';
 import Switch from '@mui/material/Switch';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormGroup from '@mui/material/FormGroup';
@@ -45,7 +45,7 @@ export default function Navbar(props) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {props.title}
+            {props.title} - Using Redux
           </Typography>
           {auth && (
             <div>
